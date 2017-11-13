3ce7a50c66c55ca4031860e607575c4864a1fb62
Jiwon Park <jiiwon0@gmail.com>
2017-11-13 13:53:39 -0800

    initial deploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-css/semantic.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
