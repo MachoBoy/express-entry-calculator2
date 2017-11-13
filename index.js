107cc8a2726e3592713581ecd5107f95c67a86c5
Jiwon Park <jiiwon0@gmail.com>
2017-11-13 14:02:51 -0800

    initial deploy

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-app.get('/api', (req, res) => {
-  res.set('Content-Type', 'application/json');
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + 'client', 'build', 'index.html'));
+  });
+}
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname + 'client', 'build', 'index.html'));
-});
-
-app.listen(process.env.PORT || 5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
