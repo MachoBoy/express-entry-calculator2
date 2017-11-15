b285492dd4d68621428336b0ddc6d31acb622095
Jiwon Park <jiiwon0@gmail.com>
2017-11-15 11:11:44 -0800

    redux-form

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import selectionReducer from './selectionReducer';
+import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
-  selection: selectionReducer
+  selection: selectionReducer,
+  form: formReducer
 });
